42c87a03d02f30e28e8484a92f17a9a6fea05561
steakinzer <clementjodin1u@gmail.com>
2023-02-13 16:38:25 +0100

    second push

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,15 @@ class HomeController < ApplicationController
   def welcome
   p params
   @first_name = params[:first_name]
+  @gossip = Flopssip.all
+  @user = User.all
+  end
+
+  def show
+    @gossip = Flopssip.all
+    @user = User.all
+    @id = params[:id]
 
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'contact/' => 'contact#index_contact'
   get 'team/' => 'team#index_team'
   get 'welcome/:first_name' => 'home#welcome'
+  get 'potin/:id' => 'home#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
