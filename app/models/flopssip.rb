52322a4d296316e2b14a5599530542157ff22763
steakinzer <clementjodin1u@gmail.com>
2023-02-14 19:01:43 +0100

    push securité

--- app/models/flopssip.rb
+++ app/models/flopssip.rb
@@ -2,4 +2,9 @@ class Flopssip < ApplicationRecord
   belongs_to :user
   has_many :join_table_tag_flopssips
   has_many :tags, through: :join_table_tag_flopssips
+  validates :title, 
+  length: {in: 3..14},
+  presence: true
+  validates :content,
+  presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'welcome/:first_name' => 'home#welcome'
   get 'potin/:id' => 'home#show', as: "potin_id"
   get 'author/:author' => 'author#author', as: "author_id"
+  resources :gossips
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
